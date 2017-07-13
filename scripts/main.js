34160b41cf9ae8948b53a2089e0b96280eeb70dc
Taylor Cox <taylorashtoncox@gmail.com>
2017-07-13 15:33:27 -0400

    Completed project

--- scripts/main.js
+++ scripts/main.js
@@ -12,29 +12,34 @@
 function handValue (hand) {
   // start at total of 0
   let total = 0
+
   // iterate through hand
+  for (i = 0; i < hand.length; i++) {
+    // if not an ace or face card, add the value of the card
+    if ((hand[i] != "K") && (hand[i] != "Q") && (hand[i] != "J") && (hand[i] != "A")) {
+      total += parseInt(hand[i], 10)
+    }
+  }
+
   for (i = 0; i < hand.length; i++) {
     // if face card exists, add 10 to the total
     if ((hand[i] === "K") || (hand[i] === "Q") || (hand[i] === "J")) {
       total += 10
-    } else {
-      // if face card doesn't exist, add value of card to total
-      total += hand[i]
     }
+  }
+
+  for (i = 0; i < hand.length; i++) {
     // if ace exists and the total of the cards plus 11 will be over 21, add 1 to the total
     if ((hand[i] === "A") && (total + 11 > 21)) {
       total += 1
     // if ace exists and the total of the cards plus 11 will be less than or equal to 21, add 11 to the total
     } else if ((hand[i] === "A") && (total + 11 <= 21)) {
-      total += 1
+      total += 11
     }
-    // return the total
-    return total;
   }
+  // return the total
+  return total;
 }
-
-
-
 
 /* -----  Hints ------
 
